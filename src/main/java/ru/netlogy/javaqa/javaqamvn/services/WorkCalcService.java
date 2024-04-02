52be0d3c2893815bd03dcc889f0ed0fa0f34382c
Nikita Chalyi <nikita4556@gmail.com>
2024-04-03 00:33:08 +0300

    parametrized

---
+++ src/main/java/ru/netlogy/javaqa/javaqamvn/services/WorkCalcService.java
@@ -0,0 +1,18 @@
+package ru.netlogy.javaqa.javaqamvn.services;
+
+public class WorkCalcService {
+    public int calculate(int income, int expenses, int treshold) {
+        int money = 0; // количество денег на счету
+        int count = 0;
+        for (int month = 1; month < 12; month++) {
+            if (money >= treshold) { // можем ли отдыхать?
+                count++; // увеличиваем счётчик месяцев отдыха
+                money = money - expenses;
+                money = money / 3;
+            } else {
+                money = money + income - expenses;
+            }
+        }
+        return count;
+    }
+}
